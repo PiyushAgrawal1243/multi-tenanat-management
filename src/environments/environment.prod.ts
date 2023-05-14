c87fb361e19754083b1f4ccdc2ba02545abc9e05
PiyushAgrawal123 <piyush.agarwal@knoldus.com>
2023-05-14 07:59:50 +0530

    multitenant-app deployement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   appVersion: '1.0.0',
   api: {
+    
     baseUrl: 'http://localhost:5000/',
     routes: {
       login: {endpoint: 'login', method: 'POST'},
